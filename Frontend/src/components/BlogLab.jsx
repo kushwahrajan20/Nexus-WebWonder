6c4b28cc54f48208514ca02a3ec296ea00464a9f
krajan2006 <kushawahaharikishan1@gmail.com>
2024-07-07 14:03:57 +0530

    blog page blogs are added

--- Frontend/src/components/BlogLab.jsx
+++ Frontend/src/components/BlogLab.jsx
@@ -6,7 +6,7 @@ import list from '../../public/blogs.json'
 function BlogLab() {
     return (
         <>
-            <div className="mt-10 grid grid-cols-1 md:grid-cols-3">
+            <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 md:flex-row my-7 mt-10 grid grid-cols-1 md:grid-cols-3">
                 {
                     list.map((item) => (<CardsBlog key={item.id} item={item} />))
                 }
